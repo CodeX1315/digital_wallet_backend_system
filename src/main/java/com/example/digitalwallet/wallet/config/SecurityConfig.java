d2673ee6cc070baf09700ff1264362b4bd50d483
CodeX1315 <deepakmahade777@gmail.com>
2026-06-27 12:26:33 +0530

    fix: swagger config

--- src/main/java/com/example/digitalwallet/wallet/config/SecurityConfig.java
+++ src/main/java/com/example/digitalwallet/wallet/config/SecurityConfig.java
@@ -48,6 +48,7 @@ public class SecurityConfig {
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
                                 "/docs/**",
+                                "/webjars/**",
                         "/docs").permitAll()
                         .requestMatchers("/user/**").authenticated()
                         .requestMatchers("/admin/**").hasAuthority("ADMIN")
